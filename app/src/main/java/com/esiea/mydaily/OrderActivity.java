74e876ce1bc412666354ca358fa87f1fe43c54b2
Richy <richardjunior.homsikue@esprit.tn>
2018-05-24 23:28:08 +0200

    Ajout de l'autocomplete
    J'ai ajouté l'autocomplete , il reconnait très bien les adresses mais il ne localise pas encore.

--- app/src/main/java/com/esiea/mydaily/OrderActivity.java
+++ app/src/main/java/com/esiea/mydaily/OrderActivity.java
@@ -1,18 +1,50 @@
 package com.esiea.mydaily;
 
 import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.common.api.CommonStatusCodes;
+
 import java.lang.Throwable;
 import java.lang.Object;
-
 public class OrderActivity extends AppCompatActivity {
+    Notifications notifications=new Notifications();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+               // Log.i(TAG, "Place: " + place.getName());
+                String placeName=place.getName().toString();
+                notifications.notificationFunction(getApplicationContext(), "toast" , ""+placeName);
+
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
+
+
+        });
     }
 
 
