4fb04515b6af11a4dc0fdeebc98f41d05dea90ff
Richy <richardjunior.homsikue@esprit.tn>
2018-05-31 00:01:37 +0200

    Menu

--- app/src/main/java/com/esiea/mydaily/SettingActivity.java
+++ app/src/main/java/com/esiea/mydaily/SettingActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -217,6 +219,26 @@ public class SettingActivity extends AppCompatActivity {
         if (authListener != null) {
             auth.removeAuthStateListener(authListener);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_off_app:
+                Notifications notifications=new Notifications();
+                notifications.notificationFunction(SettingActivity.this , "toast" , "je suis le menu");
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
